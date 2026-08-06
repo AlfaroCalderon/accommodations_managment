4d4b72ce7899a0c908e7607dd23d605b6de8d306
AlfaroCalderon <rodri.alfaro16@gmail.com>
2026-08-06 15:43:05 -0600

    refactor: Add viewport meta tag for improved mobile responsiveness in dashboard

--- dashboard.php
+++ dashboard.php
@@ -36,6 +36,7 @@ foreach($likedAccommodations as $fav){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accommodations Dashboard</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/dashboard_styles.css">
