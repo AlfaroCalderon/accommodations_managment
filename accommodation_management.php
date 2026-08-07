55062193eeb1f7a489f8ac6333d5c91f42f81111
AlfaroCalderon <rodri.alfaro16@gmail.com>
2026-08-06 19:36:49 -0600

    refactor: Add Google Fonts link for improved typography in accommodation management

--- accommodation_management.php
+++ accommodation_management.php
@@ -51,6 +51,7 @@ ob_clean(); // Clear any previous output
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/accommodation_management.css">
     <link rel="stylesheet" href="css/navbar.css">
     <title>Document</title>
